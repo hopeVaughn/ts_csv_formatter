fa679850382afbda1520c8c06d608bab34a1cead
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-19 14:48:01 -0700

    add csvreader class

--- build/index.js
+++ build/index.js
@@ -1,34 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = __importStar(require("fs"));
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8',
-})
-    .split('\n')
-    .map((row) => {
-    return row.split(',');
-});
-console.log(matches);
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+;
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins}`);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import { match } from 'assert';
-import * as fs from 'fs';
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8',
-})
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+import { CsvFileReader } from "./CsvFileReader";
+const reader = new CsvFileReader('football.csv')
+reader.read();
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++
+  }
+}
+console.log(`Man United won ${manUnitedWins}`);
