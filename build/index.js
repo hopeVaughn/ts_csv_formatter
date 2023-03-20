ecfa6e843631fa9031318651275b2c1fbd9391e8
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-20 11:15:44 -0700

    add output to html

--- build/index.js
+++ build/index.js
@@ -2,13 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
 const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
+const HtmlReports_1 = require("./reportTargets/HtmlReports");
 // Create and object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 // Create an instance of 'MatchReader' and pass in something that satisfies the 'DataReader' interface
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man City'), new HtmlReports_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from "./CsvFileReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReports";
 // Create and object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv')
 // Create an instance of 'MatchReader' and pass in something that satisfies the 'DataReader' interface
@@ -11,8 +12,8 @@ matchReader.load();
 
 
 const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport(),
+  new WinsAnalysis('Man City'),
+  new HtmlReport(),
 );
 
 summary.buildAndPrintReport(matchReader.matches)
